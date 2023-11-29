2879a6b48e9e1e2fe94acfcfcea45ef2c959a0f4
Laurent-Tourtet <laurent.tourtet@oclock.school>
2023-11-29 17:49:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function updateLocalStorage(messages) {
+    localStorage.setItem('chat-messages', messages.innerHTML);
+}
+
+function sendMessage() {
+    const input = document.getElementById('chat-input');
+    let messages = document.getElementById('chat-messages');
+
+    let message = document.createElement('div');
+    let text = document.createElement('p');
+    text.textContent = input.value;
+    message.appendChild(text);
+
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.textContent = 'Supprimer';
+    deleteButton.onclick = function(e) {
+        e.target.parentNode.remove();
+        updateLocalStorage(messages);
+    };
+    
+    message.appendChild(deleteButton);
+
+    messages.appendChild(message);
+
+    updateLocalStorage(messages);
+
+    input.value = '';
+}
+
+function resetMessages() {
+    let messages = document.getElementById('chat-messages');
+    messages.innerHTML = '';
+    updateLocalStorage(messages);
+}
+
